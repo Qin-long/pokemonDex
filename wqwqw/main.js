d90efc64e7abf7cec111ef283b379116f4bfa457
qinglong <qingfeng@email>
2024-05-10 21:31:39 +0800

    update

--- wqwqw/main.js
+++ wqwqw/main.js
@@ -7,43 +7,35 @@ import { $http } from '@escook/request-miniprogram'
 // 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
 uni.$http = $http
 // 基地址
-$http.baseUrl = 'http://172.220.5.169:3000/'
+$http.baseUrl = 'http://localhost:3000/'
 
 //请求开始做的事情
-$http.beforeRequest = function(options){
+$http.beforeRequest = function (options) {
 	uni.showLoading({
-		title:'数据加载中'
+		title: '数据加载中'
 	})
-	 // 判断请求的是否为有权限的 API 接口
-	  // if (options.url.indexOf('/my/') !== -1) {
-	  //   // 为请求头添加身份认证字段
-	  //   options.header = {
-	  //     // 字段的值可以直接从 vuex 中进行获取
-	  //     Authorization: store.state.m_user.token,
-	  //   }
-	  // }
 }
 // 请求完成之后做一些事情
 $http.afterRequest = function (options) {
-  uni.hideLoading()
-  if (options.data.code == 200){
-	   options.data = options.data.data
-  }else{
-	uni.$showMsg()
-  }
+	uni.hideLoading()
+	if (options.data.code == 200) {
+		options.data = options.data.data
+	} else {
+		uni.$showMsg()
+	}
 }
 // 封装的展示消息提示的方法
 uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
-  uni.showToast({
-    title,
-    duration,
-    icon: 'none',
-  })
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none',
+	})
 }
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App,
+	...App,
 	// 挂载vuex到vue上
 	store
 })
@@ -53,9 +45,9 @@ app.$mount()
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
